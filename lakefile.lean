fed7b48e16f4563e82795a857d5ad47d69712d18
adamtopaz <github@adamtopaz.com>
2024-05-06 11:53:28 -0600

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package «SpecGraph» where
+  -- add package configuration options here
+
+@[default_target]
+lean_lib «SpecGraph» where
+  -- add library configuration options here
