b24640bfe1dbb0a3202dd3471869537ba60179a7
adamtopaz <github@adamtopaz.com>
2024-05-08 14:21:16 -0600

    thing

--- SpecGraph.lean
+++ SpecGraph.lean
@@ -2,4 +2,17 @@ import SpecGraph.Widget
 
 universe u
 
+/--
+We introduce a natural number $a$, and set it equal to $0$.
+-/
+@[spec_decl]
+def a := 0
+
+/--
+Now we introduce another natural number $b$,
+which is just the sum of $a$ and $0$.
+-/
+@[spec_decl]
+def b := a + 0
+
 #spec_graph
